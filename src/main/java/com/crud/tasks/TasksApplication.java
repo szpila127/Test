11b997109989596d2f2a4dc6615ef0694ad816e0
szpila127 <inglot.seb@gmail.com>
2019-09-25 21:34:48 +0200

    18.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,6 +1,5 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto();
 		SpringApplication.run(TasksApplication.class, args);
 	}
-
 }
