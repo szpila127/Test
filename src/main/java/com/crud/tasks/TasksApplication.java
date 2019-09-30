a26d27eeada7c6629ae26087eaf524bb30a9768c
szpila127 <inglot.seb@gmail.com>
2019-09-30 23:29:31 +0200

    19.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TasksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(TasksApplication.class, args);
 	}
 }
