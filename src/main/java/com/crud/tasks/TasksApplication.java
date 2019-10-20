95d02114ba9b2e06ec5818b2943be0d0b6cd87fd
szpila127 <inglot.seb@gmail.com>
2019-10-20 03:04:37 +0200

    21.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,11 +8,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) { SpringApplication.run(TasksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApplication.class);
+    }
 }
