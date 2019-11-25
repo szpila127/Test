24e408fecdbb2dc707d95d529249818a6d19737e
szpila127 <inglot.seb@gmail.com>
2019-11-25 23:48:06 +0100

    TrelloClientProblem

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,54 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class TrelloClient {
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/sebastianinglot/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .build()
+                .encode()
+                .toUri();
+
+//        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject("https://api.trello.com/1/members/sebastianinglot/boards?key=4a3548f428e3b44267d1006be08182e0&token=bce1d0c620988cdecbebc930fc90b01b48f5f0ad81ff4f000532f0fa958d0a3a&fields=name,id", TrelloBoardDto[].class);
+
+        if (boardsResponse != null) {
+            return Arrays.asList(boardsResponse);
+        }
+        System.out.println(url);
+        return new ArrayList<>();
+
+
+//        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(
+//                trelloApiEndpoint + "/members/sebastianinglot/boards"
+//                        + "?key=" + trelloAppKey + "&token=" + trelloToken, TrelloBoardDto[].class);
+    }
+}
