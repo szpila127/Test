338cf7e6eb16286fb3dd570525047e93a0fa47e8
szpila127 <inglot.seb@gmail.com>
2020-01-04 21:27:37 +0100

    29.1

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -33,7 +33,7 @@ public class TrelloService {
     public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
-                "New card: " + card.getName() + " has been created on your Trello account")));
+                "New card: " + card.getName() + " has been created on your Trello account.")));
         return newCard;
     }
 }
